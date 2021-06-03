1acc009a405ac1f1f1d069a9619bfcac385742de
Alisson Ferreira Evangelista <82222646+AlissonFerreiraEvangelista@users.noreply.github.com>
2021-06-03 17:04:07 -0300

    Segunda
    Criação do Login e Tela de Busca

---
+++ script.js
@@ -0,0 +1,6 @@
+
+window.onload =	function() { document.getElementById('resultado-fildset').style.visibility = 'hidden' };
+
+function Seach(){
+	document.getElementById('resultado-fildset').style.visibility = 'visible';
+}
