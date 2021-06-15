5d46bb4da429c773fe8afef54d4053583139d8fd
Alisson Ferreira Evangelista <82222646+AlissonFerreiraEvangelista@users.noreply.github.com>
2021-06-14 22:11:35 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-
+/*Eventos da Pesquisa*/
 window.onload =	function() { document.getElementById('resultado-fildset').style.visibility = 'hidden' };
 
 function Seach(){
 	document.getElementById('resultado-fildset').style.visibility = 'visible';
 }
+/* Eventos do Menu */
+document.getElementById('consulta').addEventListener('click', function(){
+  window.location.href = "buscar.html"
+})
+document.getElementById('agenda').addEventListener('click', function(){
+  window.location.href = "Agenda.html"
+})
+document.getElementById('relatorio').addEventListener('click', function(){
+  window.location.href = "relatorio.html"
+})
+/**/
+document.getElementById('Inclui').addEventListener('click', function(){
+  document.getElementById('Inclui').readOnly = true;
+})
+
+
 
     
 
